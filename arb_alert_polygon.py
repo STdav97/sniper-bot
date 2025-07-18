2244ceeb04c4919ded02e2f19cf946965e4081a5
STdav97 <stilldav97@gmail.com>
2025-07-18 14:05:29 +0200

    Update arb_alert_polygon.py

--- arb_alert_polygon.py
+++ arb_alert_polygon.py
@@ -1,16 +1,1 @@
-from aiogram import Bot
-import os
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # Mets ton chat_id ici (int)
-
-bot = Bot(token=TOKEN)
-
-async def send_arb_alert(msg):
-    await bot.send_message(CHAT_ID, msg)
-import asyncio
-
-# ... dans ta boucle principale
-if opportunity_detected:
-    message = "🚨 ARBITRAGE DETECTÉ SUR POLYGON !"
-    asyncio.run(send_arb_alert(message))
